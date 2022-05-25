76ada277f51745f73f4688e9be93561cc1df4b4d
kkt <kkt4828@kakao.com>
2022-05-26 06:35:40 +0900

    [REFACTOR] App.js 코드 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Home from './components/Home/Home'
 import Log from './components/Log/Log'
-import CropDetail from "./components/CropDetail/CropDetail";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import LogDetail from "./components/LogDetail/LogDetail";
 import Login from "./components/Login/Login";
@@ -13,7 +12,6 @@ function App() {
                 <Route path='/login' element={<Login />} />
                 <Route path='/' element={<Home />} />
                 <Route path='/log' element={<Log />} />
-                <Route path='/cropdetail' element={<CropDetail />} />
                 <Route path='/logdetail' element={<LogDetail />} />
             </Routes>
         </Router>
