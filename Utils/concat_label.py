1b2e54965f95491efa205fe7d30d61fc0153808c
Changhyun Kim <78781975+variety82@users.noreply.github.com>
2022-05-26 16:06:47 +0900

    Revert "[FEAT] Add MMClassication Baseline, Utils"

--- Utils/concat_label.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_csv('/opt/ml/input/project/data/Validation/abnormal_label.csv')
-df2 = pd.read_csv('/opt/ml/input/project/data/Validation/normal_label.csv')
-
-df = pd.concat([df1,df2])
-
-df.to_csv('./validation_label.csv',sep=',',index=False)
--- mmclassification/configs/1.MyConfig/swin_transformer/swin-large_16xb64_in1k-384px.py
+++
@@ -1,6 +0,0 @@
-# Only for evaluation
-_base_ = [
-    '../../_base_/models/swin_transformer/large_384.py',
-    '../datasets/custom.py',
-    '/opt/ml/input/Project/mmclassification/configs/_base_/schedules/cub_bs64.py', '../default_runtime.py'
-]
