b65742e1a94305ef67f37807570c3883ca60421f
park95gr <pkr951113@gmail.com>
2022-06-01 20:14:48 +0900

    feat : Add db 일지 전송 기능 추가

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+from urllib import response
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
@@ -50,15 +51,27 @@ async def login(user: user):
 # 테스트 부분
 # upload_file('./호랑이.jpeg', 'smartfarmtv')
 objects_list = make_objects_list('smartfarmtv')
-tiger = make_object('smartfarmtv', objects_list[-1])
+tiger = make_object('smartfarmtv', objects_list[0])
 date, input_datetime = get_image_date(tiger)
 img_url = get_image_url(tiger)
 
 tw = weather.today_weather(date, input_datetime, 60, 120)  # 날짜, 시간, 위치, 경도
 
+# @app.post('/api/v1/postDisease')
+# async def postDisease():
+#     response = [{"idx": 1, "category": "disease", "date": date, "kind": "병", "datetime": [{'id': '1', 'datetime': input_datetime}], "weather": [{'state' : tw['state'], 'temperature' : tw['temperature'], "precipitation": tw["precipitation"]}], "image_url": img_url}]
+#     return {
+#         "diseases": response
+#     }
+
 @app.post('/api/v1/postDisease')
 async def postDisease():
-    response = [{"idx": 1, "category": "disease", "date": date, "kind": "병", "datetime": [{'id': '1', 'datetime': input_datetime}], "weather": [{'state' : tw['state'], 'temperature' : tw['temperature'], "precipitation": tw["precipitation"]}], "image_url": img_url}]
+    f = open('database.csv', 'r', encoding="utf-8")
+    rd = csv.reader(f)
+    for line in rd:
+        if line[0] == '1_1':
+            response = [{"idx": 1, "category": "disease", "date": date, "kind": "병", "datetime": [{'id': '1', 'datetime': input_datetime}], "weather": [{'state' : tw['state'], 'temperature' : tw['temperature'], "precipitation": tw["precipitation"]}], "image_url": img_url, 'dbmemo' : line[-1]}]
+    f.close()
     return {
         "diseases": response
     }
@@ -72,15 +85,23 @@ async def postWeather():
 
 @app.post('/api/v1/postMemo')
 async def postMemo(memo: memo):
-    print(memo)
     f = open('database.csv', 'a', newline='', encoding="utf-8")
     wr = csv.writer(f)
     
-    print(memo.datetime)
     idx = str(memo.idx) + '_' + str(memo.datetime["id"])
     wr.writerow([idx, memo.crop, memo.bug, memo.weather, memo.memo])
     f.close()
-    
 
     return {'response': "access"}
 
+
+# @app.post('/api/v1/postDBMemo')
+# async def postDBMemo():
+#     f = open('database.csv', 'r', encoding="utf-8")
+#     rd = csv.reader(f)
+#     for line in rd:
+#         if line[0] == '1_1':
+#             response = [{'dbmemo' : line[-1]}]
+#     f.close()
+
+#     return {'dbmemo' : response}
