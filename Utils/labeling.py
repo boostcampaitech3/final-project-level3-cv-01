11f9268662522d4c35e2000fd160283baf27fad0
mata1139 <pms1139@gmail.com>
2022-06-02 18:45:21 +0900

    feat : #29 Modify Utils

--- Utils/labeling.py
+++ Utils/labeling.py
@@ -1,9 +1,9 @@
 import os
 import json
 import pandas as pd
-ann_folder = '/opt/ml/input/Project/data/Validation/label'
+ann_folder = '/opt/ml/input/Project/data/Training/label'
 ann_files = os.listdir(ann_folder)
-image_folder = '/opt/ml/input/Project/data/Validation/image'
+image_folder = '/opt/ml/input/Project/data/Training/image'
 label_data = []
 
 
@@ -27,28 +27,30 @@ for file in ann_files:
 
         elif risk == 2: # 중기
             #label = 2
-            label=1
+            label = 1
 
         else : # 말기
             #label =3
-            label=1
+            label = 1
     
     else: # 배추노균병
 
         if risk == 1:   # 초기
             #label = 4
-            label=2
+            label = 2
 
         elif risk == 2: # 중기
             #label = 5
+            label = 2
 
         else :  # 말기
-            label = 6
+           # label = 6
+            label = 2 
 
     label_data.append([os.path.join(image_folder,fname),label])
 
 df = pd.DataFrame(label_data,
                         columns = ['path','label'])
 
-df.to_csv('./validation.csv',sep=',',columns=['path','label'],index=False)
+df.to_csv('./train_3label.csv',sep=',',columns=['path','label'],index=False)
     
