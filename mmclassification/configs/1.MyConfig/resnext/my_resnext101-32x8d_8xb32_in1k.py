1a5f68ed2e8c19ecd7cc61ec9790c39e651e56e7
mata1139 <pms1139@gmail.com>
2022-06-07 16:34:45 +0900

    refator : refactor capital letter

--- mmclassification/configs/1.MyConfig/resnext/my_resnext101-32x8d_8xb32_in1k.py
+++
@@ -1,8 +0,0 @@
-_base_ = [
-    '../../_base_/models/resnext101_32x8d.py',
-    '../datasets/custom.py',
-    #'../schedules/AdamW.py', 
-    '/opt/ml/input/Project/mmclassification/configs/_base_/schedules/cub_bs64.py',
-    
-    '../default_runtime.py'
-]
